6c57efbf17a0c9f31186db368476dd769f427d59
babe25 <babehndr5@gmail.com>
2024-09-06 04:29:13 +0700

    Add Online Count

--- src/main.js
+++ src/main.js
@@ -13,14 +13,13 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 
   // Event untuk bergabung ke room tertentu
-  socket.on("joinRoom", (roomName) => {
+  socket.on("joinRoom", (roomName, user) => {
     if (!socket.rooms.has(roomName)) {
       socket.join(roomName);
-      io.to(roomName).emit("chat message", `A new user has joined ${roomName}`);
+      io.to(roomName).emit("chat message", `${user} has joined ${roomName}`);
+      const sizeRooms = io.sockets.adapter.rooms.get(roomName);
+      io.to(roomName).emit("online", sizeRooms.size);
     }
-
-    // const rooms = io.sockets.adapter.rooms.get(roomName);
-    // console.log(`${roomName}: ${Array.from(rooms)}`);
   });
 
   socket.on("leaveRoom", (roomName) => {
@@ -30,17 +29,21 @@ io.on("connection", (socket) => {
       `User ${socket.id} left room: ${roomName}`
     );
     socket.leave(roomName);
-    console.log(`User ${socket.id} left room: ${roomName}`);
   });
 
   // Event untuk mengirim pesan ke room tertentu
-  socket.on("chat message", (message, roomName) => {
+  socket.on("chat message", (message, roomName, user) => {
     console.log(`message: ${message} sent to room: ${roomName}`);
 
-    io.to(roomName).emit("chat message", message);
+    io.to(roomName).emit("chat message", `${user} : ${message}`);
   });
 
   socket.on("disconnect", () => {
+    io.emit("chat message", `User ${socket.id} left the chat`);
+    // io.to(socket.rooms.)
+    // socket.rooms.forEach((room) => {
+    //   console.log(room);
+    // });
     console.log("user disconnected");
   });
 });
